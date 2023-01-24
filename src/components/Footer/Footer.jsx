381639462a0d7a5824d18a020df44c1a3272bcb1
Aziza H <mess.aziza@gmail.com>
2023-01-24 07:16:29 +0100

    installer rechart

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,27 +6,26 @@ import syclisme from "../../assets/syclisme.svg";
 import poids from "../../assets/poids.svg";
 
 export default function Footer() {
-  return (
-    <footer>
-      <div className="container">
-        <div></div>
-        <div className="nav-footer">
-          <Link to="">
-            <img src={yoga} alt="yoga" />
-          </Link>
-          <Link to="">
-            <img src={natation} alt="natation" />
-          </Link>
-          <Link to="">
-            <img src={syclisme} alt="syclisme" />
-          </Link>
-          <Link to="">
-            <img src={poids} alt="poids" />
-          </Link>
+	return (
+		<footer>
+			<div className="container">
+				<div className="nav-footer">
+					<Link to="">
+						<img src={yoga} alt="yoga" />
+					</Link>
+					<Link to="">
+						<img src={natation} alt="natation" />
+					</Link>
+					<Link to="">
+						<img src={syclisme} alt="syclisme" />
+					</Link>
+					<Link to="">
+						<img src={poids} alt="poids" />
+					</Link>
 
-        </div>
-        <p> Copyright, SportSee 2022</p>
-      </div>
-    </footer>
-  );
+				</div>
+				<p> Copyright, SportSee 2022</p>
+			</div>
+		</footer>
+	);
 }
