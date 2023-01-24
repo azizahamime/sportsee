913651b5a6ab4e0446865b1b666e77e1d0e97d33
Aziza H <mess.aziza@gmail.com>
2023-01-24 05:38:01 +0100

    importer les données nutrition

--- src/components/connection.js
+++ src/components/connection.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const urlUsers = "http://localhost:3001/user/20";
+const urlUsers = "http://localhost:3001/user/12";
 
 export default function Connection(url = urlUsers) {
 	const [data, setData] = useState({});
--- src/pages/Profil/Profil.jsx
+++ src/pages/Profil/Profil.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {  useParams } from "react-router-dom";
 import Connection from "../../components/connection";
 import properties from "../../properties";
+import calories from "../../assets/calories-icon.svg";
+import proteines from "../../assets/protein-icon.svg";
+import carbs from "../../assets/carbs-icon.svg";
+import fat from "../../assets/fat-icon.svg";
+
 
 
 
@@ -15,15 +20,60 @@ export default function Profil() {
   
 	return (
 		<main>
-			{isLoading ?(
-				<div> Téléchargement</div>
-			):(
-				<h1> Bonjour {data.data.userInfos.firstName}</h1>
+			<div className="container">
+				{isLoading ?(
+					<div> Téléchargement</div>
+				):(
+					<Fragment>
+						<div className="profil-header">
+							<h1> Bonjour <span className="name">{data.data.userInfos.firstName}</span></h1>
+							<p> Félicitation ! Vous avez explosé vos objectifs hier 👏 </p>
+						</div>
+						<div className="profile-body">
+							
+							<div className="charts">
+								<div className="activity"></div>
+								<div className="mini-charts">
+									<div className="sessions"></div>
+									<div className="performance"></div>
+									<div className="score"></div>
+								</div>
+							</div>
+							<div className="nutrition">
+								<div className="calories">
+									<img src={calories} alt="calories" />
+									<div>
+										<p className="amount">{data.data.keyData.calorieCount}</p>
+										<p className="name">Clories</p>
+									</div>
+								</div>
+								<div className="proteines">
+									<img src={proteines} alt="proteines" />
+									<div>
+										<p className="amount">{data.data.keyData.proteinCount}</p>
+										<p className="name">Proteines</p>
+									</div>
+								</div>
+								<div className="glucides">
+									<img src={carbs} alt="glucides" />
+									<div>
+										<p className="amount">{data.data.keyData.carbohydrateCount}</p>
+										<p className="name">Glucides</p>
+									</div>
+								</div>
+								<div className="lipides">
+									<img src={fat} alt="lipides" />
+									<div>
+										<p className="amount">{data.data.keyData.lipidCount}</p>
+										<p className="name">Lipides</p>
+									</div>
+								</div>
+							</div>						
+						</div>
+					</Fragment>
+				)}
 
-			)}
-
-      
+			</div>
 		</main>
-
 	);
 }
